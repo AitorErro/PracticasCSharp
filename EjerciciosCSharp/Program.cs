293ca8317972ad26bf4b567785c2b9449a37ea57
AitorErro <aitor.e.4.a@gmail.com>
2017-07-24 21:01:18 +0200

    Fusion de desarrolloPortatil a Master del día 24/07/2017

--- EjerciciosCSharp/Program.cs
+++ EjerciciosCSharp/Program.cs
@@ -930,7 +930,7 @@ namespace EjerciciosCSharp
 
             try
             {
-                //Variables
+                //Variabless
                 int a = 0;
                 int b = 1;
                 int c = 3;
